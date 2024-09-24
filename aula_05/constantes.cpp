296ffc06241acf22760cfe6583cfff418906a5dd
LuisGomes18 <luiscgomes0@gmail.com>
2024-09-24 16:22:02 +0100

    Adição da aula 05

---
+++ aula_05/constantes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+#define PI 3.1415
+#define nome cout << "luis\n";
+
+int main() {
+    /*int vidas;
+    int municao;
+    int vida;*/
+
+    int vidas=12, municao=20, vida=100;
+
+    /*int vidas, int municao, int vida;*/
+
+    cout << PI << "\n";
+    nome
+
+    return 0;
+}
